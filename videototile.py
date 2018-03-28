799bb29844a972fc7320a7a9184416911a66a90e
Connor Monahan <cma2714@gmail.com>
2018-03-28 18:01:09 -0500

    Use raspberry pi camera for input

--- videototile.py
+++ videototile.py
@@ -1,23 +1,33 @@
 import cv2
 import tile
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+
 
 board = tile.TileArray(rows=1, cols=1, height=10, width=10)
 leds = tile.LEDStrip(board)
 
-cap = cv2.VideoCapture(0)
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
 
-while cap.isOpened():
-    cap.grab()
-    _, image = cap.retrieve()
+time.sleep(0.1)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    image = frame.array
 
     crop = image[0:image.shape[1], :, :]
     resize = cv2.resize(crop, (10, 10))
     leds.draw(resize)
+
+    rawCapture.truncate(0)
 
     cv2.imshow('Output', resize)
     key = cv2.waitKey(1)
     if key == 27:
         break
 
-
+print("Quitting application")
 cv2.destroyAllWindows()
