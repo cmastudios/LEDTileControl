6a88669d0468c3750f814237c6d2771d766dc3fd
Connor Monahan <cma2714@gmail.com>
2018-11-16 17:20:31 -0600

    Configurable font size
    Closes #9

--- patterns/text.py
+++ patterns/text.py
@@ -4,15 +4,19 @@ import numpy as np
 i = 0
 
 
-def display(board, leds, message="IEEE", increment=1, delay=0.1):
+def display(board, leds, message="IEEE", increment=1, delay=0.1, fsize=None):
     global i
     message = message.replace("_", " ")
     increment = int(increment)
     delay = float(delay)
+    if fsize is None:
+        fsize = 0.075 * board.shape[0]
+    else:
+        fsize = float(fsize)
     # create blue canvas
     img = np.tile([0, 0, 255], board.shape).astype(np.uint8)
     # draw red message
-    cv2.putText(img, message, (board.shape[1] - i, board.shape[0] - 2), cv2.FONT_HERSHEY_PLAIN, 0.075 * board.shape[0],
+    cv2.putText(img, message, (board.shape[1] - i, board.shape[0] - 2), cv2.FONT_HERSHEY_PLAIN, fsize,
                 (255, 0, 0), int(0.1 * board.shape[0]))
     # display
     leds.draw(img, delay=delay)
